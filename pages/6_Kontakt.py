bbf205ca2db078b9fe297cfc7384230be6544efe
silvandario <147420237+silvandario@users.noreply.github.com>
2023-12-06 15:38:00 +0100

    Update 6_Kontakt.py
    disclaimer update

--- pages/6_Kontakt.py
+++ pages/6_Kontakt.py
@@ -14,7 +14,7 @@ image_file_path = 'silvan.png'
 st.image(image_file_path, caption='Schreibe Silvan auf LinkedIn')
 
 with st.expander("Weitere Informationen"):
-    st.write("Disclaimer: Diese Untersuchung wurde im Rahmen des Capstone-Projektes an der Universität St. Gallen mit dem Industriepartner Strategy& durchgeführt. Für die Richtigkeit der Daten leisten wir keine Gewähr. Weiterverwendung der Inhalte mit korrekter Zitation gestatt.")  
+    st.write("Disclaimer: Diese Untersuchung wurde im Rahmen des Capstone-Projektes an der Universität St. Gallen mit dem Industriepartner Strategy& durchgeführt. Für die Richtigkeit der Daten leisten wir keine Gewähr. Weiterverwendung der Inhalte mit korrekter Zitation gestattet.")  
 
 #hide style
 
