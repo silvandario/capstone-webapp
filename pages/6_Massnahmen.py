96a830209871b6bfd6304bf75ca3cd2e68006dbd
silvandario <147420237+silvandario@users.noreply.github.com>
2023-12-08 16:21:34 +0100

    Update 6_Massnahmen.py
    keys

--- pages/6_Massnahmen.py
+++ pages/6_Massnahmen.py
@@ -31,7 +31,7 @@ st.markdown('Folgende Massnahmen sollte Deutschland umsetzen:')
 with st.container():
     st.write('**Renteneintrittsalter**')
     st.write('Anhebung des tatsächlichen Renteneintrittsalters durch flexible Arbeitsmöglichkeiten, finanzielle Anreize und die Förderung des lebenslangen Lernens')
-    if st.checkbox('Mehr Infos'):
+    if st.checkbox('Mehr Infos zum Renteneintrittsalter'):
         pdf_file_path = 'retirement_age.pdf'
         with open(pdf_file_path, "rb") as pdf_file:
             PDFbyte = pdf_file.read()
@@ -41,7 +41,7 @@ with st.container():
 with st.container():
     st.write('**Stärkung der Langzeitarbeitslosen**')
     st.write('Überbrückung der Arbeitskräftelücke durch finanzielle Unterstützung für Unternehmen')
-    if st.checkbox('Mehr Infos'):
+    if st.checkbox('Mehr Infos zu Langzeitarbeitslosen'):
         pdf_file_path = 'longterm_unemployment.pdf'
         with open(pdf_file_path, "rb") as pdf_file:
             PDFbyte = pdf_file.read()
@@ -51,7 +51,7 @@ with st.container():
 with st.container():
     st.write('**Krankheitstage**')
     st.write('Eindämmung der steigenden Zahl von Krankheitstagen durch präventive Maßnahmen, um deren Auswirkungen auf das Arbeitsvolumen zu minimieren')
-    if st.checkbox('Mehr Infos'):
+    if st.checkbox('Mehr Infos zu Krankheitstagen'):
         pdf_file_path = 'sick_days.pdf'
         with open(pdf_file_path, "rb") as pdf_file:
             PDFbyte = pdf_file.read()
@@ -61,7 +61,7 @@ with st.container():
 with st.container():
     st.write('**Bindung**')
     st.write('Bindung von Fachkräften an das Land durch steuerliche Anreize in Form von Ermäßigungen bei den Ausbildungskosten')
-    if st.checkbox('Mehr Infos'):
+    if st.checkbox('Mehr Infos zur Bindung'):
         pdf_file_path = 'training_costs.pdf'
         with open(pdf_file_path, "rb") as pdf_file:
             PDFbyte = pdf_file.read()
@@ -71,7 +71,7 @@ with st.container():
 with st.container():
     st.write('**Asylsuchende**')
     st.write('Verbesserung der Integration von asylsuchenden Arbeitnehmern durch Vereinfachung der Asylverfahren, Vereinheitlichung der Arbeitserlaubnis und proaktive Berufsausbildung')
-    if st.checkbox('Mehr Infos'):
+    if st.checkbox('Mehr Infos zu Asylsuchenden'):
         pdf_file_path = 'asylum_seekers.pdf'
         with open(pdf_file_path, "rb") as pdf_file:
             PDFbyte = pdf_file.read()
@@ -81,7 +81,7 @@ with st.container():
 with st.container():
     st.write('**Digitalisierung**')
     st.write('Digitalisierung durch steuerliche Anreize für Unternehmen, die in innovative Technologien und die digitale Ausbildung ihrer Mitarbeiter investieren')
-    if st.checkbox('Mehr Infos'):
+    if st.checkbox('Mehr Infos zu Digitalisierung'):
         pdf_file_path = 'digitalization.pdf'
         with open(pdf_file_path, "rb") as pdf_file:
             PDFbyte = pdf_file.read()
@@ -91,7 +91,7 @@ with st.container():
 with st.container():
     st.write('**Flüchtende**')
     st.write('Erleichterung des Zugangs zu Arbeit für ukrainische Flüchtlinge durch die Anerkennung von Qualifikationen, Kinderbetreuung, Sprachkurse und eine Verringerung des bürokratischen Aufwands')
-    if st.checkbox('Mehr Infos'):
+    if st.checkbox('Mehr Infos zu Flüchtenden'):
         pdf_file_path = 'ukrainian_refugees.pdf'
         with open(pdf_file_path, "rb") as pdf_file:
             PDFbyte = pdf_file.read()
@@ -101,7 +101,7 @@ with st.container():
 with st.container():
     st.write('**Mütter fördern**')
     st.write('Förderung der Rückkehr von Müttern in den Beruf durch flexiblere Arbeitszeiten, veränderte soziale Erwartungen und bessere Kinderbetreuung')
-    if st.checkbox('Mehr Infos'):
+    if st.checkbox('Mehr Infos zu Mütter'):
         pdf_file_path = 'mothers.pdf'
         with open(pdf_file_path, "rb") as pdf_file:
             PDFbyte = pdf_file.read()
@@ -111,7 +111,7 @@ with st.container():
 with st.container():
     st.write('**Lehrlingsausbildung**')
     st.write('Stärkung der Lehrlingsausbildung durch Förderung der Gleichstellung von Männern und Frauen, unternehmerisches Engagement, soziale Anerkennung, Anreize und verbesserte Qualität')
-    if st.checkbox('Mehr Infos'):
+    if st.checkbox('Mehr Infos zur Lehrlingsausbildung'):
         pdf_file_path = 'apprenticeships.pdf'
         with open(pdf_file_path, "rb") as pdf_file:
             PDFbyte = pdf_file.read()
@@ -121,7 +121,7 @@ with st.container():
 with st.container():
     st.write('**Arbeitslosigkeit unattraktiv machen**')
     st.write('Verschärfung der Sanktionen: Erhöhung des Arbeitsvolumens durch Verschärfung der Bedingungen für den Bezug von Arbeitslosengeld')
-    if st.checkbox('Mehr Infos'):
+    if st.checkbox('Mehr Infos zur unattraktiven Arbeitslosigkeit'):
         pdf_file_path = 'unemployment.pdf'
         with open(pdf_file_path, "rb") as pdf_file:
             PDFbyte = pdf_file.read()
